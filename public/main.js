f50ed2e80559848730828fc646cb47e63876d655
banchan86 <banchan86@users.noreply.github.com>
2024-03-04 03:24:53 +0000

    deploy: 2edd1aef00c78ee7cdddd73a4b1a90d7a6157e28

---
+++ public/main.js
@@ -0,0 +1,20 @@
+// import WorkflowContainer from "./workflow.js"
+
+export default {
+    defaultTheme: 'light',
+    iconLinks: [
+        {
+            icon: 'github',
+            href: 'https://github.com/bonsai-rx/',
+            title: 'GitHub'
+        },
+        {
+            icon: 'twitter',
+            href: 'https://twitter.com/bonsai_rx/',
+            title: 'Twitter'
+        }
+        ],
+    // start: () => {
+    //     WorkflowContainer.init();
+    //}
+}
